efdd8722ab8f10f0b63ce547b6b0dadaef92afeb
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-04-15 20:04:21 +0300

    Implement edit and delete endpoints

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -24,4 +24,38 @@ app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) =>
     return Results.Created($"api/todo/{toDo.Id}", toDo);
 });
 
+app.MapPut("api/todo/{id}", async (AppDbContext context, int id, ToDo toDo) =>
+{
+    var toDoModel = await context.ToDos
+        .FirstOrDefaultAsync(t => t.Id == id);
+
+    if (toDoModel == null)
+    {
+        return Results.NotFound();
+    }
+
+    toDoModel.ToDoName = toDo.ToDoName;
+
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapPut("api/todo/{id}", async (AppDbContext context, int id, ToDo toDo) =>
+{
+    var toDoModel = await context.ToDos
+        .FirstOrDefaultAsync(t => t.Id == id);
+
+    if (toDoModel == null)
+    {
+        return Results.NotFound();
+    }
+
+    context.ToDos.Remove(toDoModel);
+
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
 app.Run();
